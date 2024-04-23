c875e560895f34804ae3ddaade1c3576874e6066
PRIYANK 1 <singhumang444@gmail.com>
2024-04-23 23:20:54 +0530

    build

---
+++ build/precache-manifest.fadead900dd0e757049d5adac03e41c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2f3ca31332225bbfb63b6ac276ad6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00900f6c518ccffb21ce",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "35ebbd1ebf09499208cd",
+    "url": "/static/js/2.84d9cc13.chunk.js"
+  },
+  {
+    "revision": "febdeaaeea8e198f18360d7dab1bb245",
+    "url": "/static/js/2.84d9cc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00900f6c518ccffb21ce",
+    "url": "/static/js/main.d5bb325e.chunk.js"
+  },
+  {
+    "revision": "4e6a3260ce0f84a09600",
+    "url": "/static/js/runtime-main.99bad934.js"
+  }
+]);
